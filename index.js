498a3066bff21e0bab743f42d489fb5ff3ffd71d
Phúc Thịnh <phucthinh0342@gmail.com>
2021-12-29 14:38:31 +0700

    a

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) =>  res.send('Hello World!'))
+app.get('/tin-tuc', (req, res) =>  res.send('tin tuc!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
